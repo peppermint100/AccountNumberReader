eedad84fea380dbd0c7b21f42cbca88aa84acfb4
peppermint100 <krpeppermint100@gmail.com>
2023-09-28 23:28:46 +0900

    카메라 사용 권한

--- AccountNumberReader/Controllers/CameraViewController.swift
+++ AccountNumberReader/Controllers/CameraViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class CameraViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        print(UserDefaults.standard.string(forKey: "leaveHistory"))
     }
 }
